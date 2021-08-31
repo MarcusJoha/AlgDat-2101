706c38dfd785416f1d613ea39b129594a8d9c869
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-08-31 09:42:46 +0200

    Task 2.1-1 done

--- src/oblig2/Main.java
+++ src/oblig2/Main.java
@@ -1,7 +1,24 @@
 package oblig2;
 
+/**
+ * S. 28, Oppg 2.1-1
+ * S. 38, Oppg 2.2-3
+ */
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(exponent(2,4));
+
     }
+
+    // Oppg 2.1-1
+    public static int exponent(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * exponent(x, n-1);
+    }
+
+
 }
